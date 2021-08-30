303347e4d35106d07d45830fe98166aa4d558e9f
shu1oka28 <40012404+shu1oka28@users.noreply.github.com>
2021-08-30 20:59:58 +0900

    Add file reading

---
+++ web/analysis_conrtroller.js
@@ -0,0 +1,7 @@
+"use strict"
+function analysis_controller(content) {
+    /* contentについて分析を行い，DOM要素を返す． */
+    var text = document.createElement('textarea');/* textarea要素を作成 */
+    text.innerHTML = content; /* 作ったtextarea要素に引数で受け取った文字列をそのまま入れる */
+    return text; /* 作ったtextarea要素を返す */
+}
---
+++ web/read_file.js
@@ -0,0 +1,37 @@
+"use strict"
+var uploadArea = document.getElementById('upload-area');
+var result = document.getElementById('result');
+var fileInput = document.getElementById('file-input');
+
+uploadArea.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    this.classList.add("over");
+}, false);
+
+uploadArea.addEventListener('dragleave', function (e) {
+    this.classList.remove("over");
+}, false);
+
+fileInput.addEventListener('change', function () {
+    resultFile(this.files[0]);
+});
+
+uploadArea.addEventListener('drop', function (e) {
+    e.preventDefault();
+    this.classList.remove("over");
+    var files = e.dataTransfer.files; //ドロップしたファイルを取得
+    if (files.length > 1) return alert('アップロードできるファイルは1つだけです。');
+    fileInput.files = files; //inputのvalueをドラッグしたファイルに置き換える。
+    resultFile(files[0]);
+}, false);
+
+function resultFile(file) {
+    /* FileReaderで読み込み、analysis_controllerを実行。結果を画面に出力 */
+    let fr = new FileReader();
+    fr.readAsText(file);
+    fr.onload = function () {
+        let analysis_result = analysis_controller(fr.result);
+        result.innerHTML = "";
+        result.appendChild(analysis_result);
+    };
+}
