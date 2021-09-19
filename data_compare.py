7d284ee256a6517b1b6ce6f81accaaa97536ab7a
maplizm <84067195+maplizm@users.noreply.github.com>
2021-09-19 22:56:09 +0900

    Change code on data_compare.py

--- data_compare.py
+++ data_compare.py
@@ -3,7 +3,7 @@ import psycopg2
 def recvDataCompare():
     #受信データとDBに保存されたデータを比較し、比較結果を返す
     """
-    connection=psycopg2.connect(host="localhost",dbname="postgres",port="5432",user="postgres",password="postgres")
+    connection=psycopg2.connect('DBのURL')
     cur = connection.cursor()
     cur.execute("SELECT COUNT(host='hostname.example.com') AS HOST_MATCH, COUNT(ip='0.0.0.0') AS IP_MATCH FROM MAIL_HEADERS;")
     results = cur.fetchall()
