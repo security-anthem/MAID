9eb5f4cfee22502a082cdcf5be4644e4a519011e
MMikipenguin <88484237+MMikipenguin@users.noreply.github.com>
2021-09-30 16:54:15 +0900

    create my data_register.py
    created my data_register.py.
    
    it just a test code (just an outline). It needs implementation.

--- data_register.py
+++ data_register.py
@@ -1,16 +1,32 @@
 import psycopg2
 import os
+import ast
 
+# parse data received from clients
+def parseData():
+    rawdata = argdata
+    parsetree = ast.parse(rawdata)
+    
+# store parsed data in each tables
+def storeData():
+
+# store data received from clients in database
 def recvDataRegister():
-    #クライアントから受信したデータをDBに格納
-    DATABASE_URL = os.environ['DATABASE_URL']
-    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-    """
+    DATABASE_URL = os.environ['DATABASE_URL'] # get database URL from export
+    connection = psycopg2.connect(DATABASE_URL, sslmode='require') # establish connection using the database URL
     cur = connection.cursor()
+
+    parseData()
+
+    storeData()
+"""
     cur.execute("INSERT INTO MAIL_HEADERS (host,ip,spf,dkim,dmarc,crypto_protocol) VALUES ('hostname.example.com','0.0.0.0','Pass','Pass','Pass','TLS');")
     cur.execute('COMMIT')
+"""
+
     cur.close()
+    connection.commit()
     connection.close()
-    
-    """
-    connection.close()
+
+if __name__ == "__main__":
+    recvDataRegister()
