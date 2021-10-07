f3f68726b114ad4efaf968cd4ac4d772c5d220c9
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2021-10-07 00:09:06 -0700

    implement received parser

--- static/parse_email.js
+++ static/parse_email.js
@@ -19,24 +19,39 @@ function parseEmail(content) {
     let body = contentArray.slice(1).join('\r\n\r\n');
     
     // Authentication-Results, Received and Delivered-Toの取得
-    let received_pattern = /(Authentication-Results:[\s\S]*?)?(Received:[\s\S]*?\+[0-9]{4}.*)([\s\S]*?Delivered-To:.*)?/g;
+    let received_pattern = /(Authentication-Results:[\s\S]*?)?(Received:[\s\S]*?[+\-][0-9]{4}.*)([\s\S]*?Delivered-To:.*)?/g;
     let received_nodes = header.match(received_pattern);
+
+    // parse結果を格納
+    var parse_result = {};
     
-    // json形式に解体する
+    // Authentication-Results, Received and Delivered-Toへ分解
     for(const node of received_nodes){
-    	if(received_pattern.test(node)){
-    	    if(/Authentication-Results/.test(node)){
-    		var authentication_results = received_pattern.exec(node)[1];
-    	    }
-    	    var received = received_pattern.exec(node)[2];
-    	    if(/Delivered-To/.test(node)){
-    		var delivered_to = received_pattern.exec(node)[3];
-    	    }
+	console.log(node);
+	let tmp = /Authentication-Results:[\s\S]*/.test(node);
+	console.log(tmp);
+	// Authentication-Resultsが存在する場合，結果をparse
+	if(/Authentication-Results:[\s\S]*/.test(node)){
+	    var authentication_results = /(Authentication-Results:[\s\S]*?)?Received:/.exec(node)[1];
+	    console.log(authentication_results);
+	    parseAuthenticationResults(authentication_results);
 	}
-    	// var res = parseReceived(received);
+	// receivedの結果をparse
+	var received = /Received:[\s\S]*?[+\-][0-9]{4}.*/.exec(node)[0];
+	console.log(received);
+	parseReceived(received);
+	
+	// Delivered-Toが存在する場合，結果をparse
+	if(/Delivered-To:.*/.test(node)){
+	    var delivered_to = /Delivered-To:.*/.exec(node)[0];
+	    console.log(delivered_to);
+	    parseDeliveredTo(delivered_to);
+	}
+	// var res = parseReceived(received);
     }
     var res = parseReceived(received);
-    
+
+    var parse_result_json = JSON.stringify(parse_result);
     let sample_parse_result_json=`
     {
         "route":[
@@ -67,6 +82,5 @@ function parseEmail(content) {
         ]
     }
     `
-    //return JSON.parse(sample_parse_result_json);
-    return res;
+    return JSON.parse(parse_result_json);
 }
